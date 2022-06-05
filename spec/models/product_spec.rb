67db73b31f6c2c8e5330fca45a0b3688de8a3dba
Greg Khmara <gkhmara@gmail.com>
2022-06-04 20:43:49 -0700

    add test for titleize that capitalizes the first letters of new products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,8 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
+  it ("titleizes the name of a product") do
+    product = Product.create({name: "cotton candy", cost: 3, country_of_origin: "USA"})
+    expect(product.name()).to(eq("Cotton Candy"))
+  end
 end
