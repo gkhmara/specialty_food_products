62e97abf374b2ad4859653263c9b9ce957e4593e
Greg Khmara <gkhmara@gmail.com>
2022-06-04 23:21:45 -0700

    add most reviews scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,14 @@ class Product < ApplicationRecord
 
   scope :three_most_recent, -> {order(created_at: :desc).limit(3)}
 
+  scope :most_reviews, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+  )}
+
   private
     def titleize_product
       self.name = self.name.titleize
