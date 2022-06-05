c6b97e2579030b760fc63532d2cd4d9a57182b1e
Greg Khmara <gkhmara@gmail.com>
2022-06-05 00:42:01 -0700

    correct amount of seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Product.destroy_all
 
 product_id_array = []
 
-5.times do |index|
+50.times do |index|
   Product.create!(name: Faker::Lorem.word, cost: Faker::Number.within(range: 1..10), country_of_origin: Faker::Lorem.word)
 
   product_id_array.push(Product.last.id)
@@ -13,7 +13,7 @@ p "Created #{Product.count} products"
 p product_id_array
 
 product_id_array.each do |num|
-  1.times do |index|
+  5.times do |index|
     Review.create!(author: Faker::Lorem.word, content_body: Faker::Lorem.paragraph_by_chars(number: 50), rating: Faker::Number.within(range: 1..5), product_id: num)
   end
 end
