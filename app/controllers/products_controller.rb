058c6fa6b317b83212b6d2d6e4d9d6c3e8b0306a
Greg Khmara <gkhmara@gmail.com>
2022-06-11 20:39:28 -0700

    updated flash message

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,6 +47,10 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def landingpage
+    @products = Product.all
+  end
+
   private
     def product_params
       params.require(:product).permit(:name, :cost, :country_of_origin)
